da0ea1f4458d4496b94f5442d985dff777df9e1c
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-01-08 17:54:30 +0100

    Initial Commit

---
+++ Meet Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meet Me
+//
+//  Created by Philipp Hemkemeyer on 08.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meet Me/Meet_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meet_MeApp.swift
+//  Meet Me
+//
+//  Created by Philipp Hemkemeyer on 08.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Meet_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
