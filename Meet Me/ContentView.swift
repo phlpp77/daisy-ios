74471201874119b18a5945e2b2e0359fae4e25ba
lukas Dech <lukasdech@gmx.de>
2021-01-09 19:03:55 +0100

    Meet Me -> MeetME

--- Meet Me/ContentView.swift
+++ Meet Me/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Meet Me")
+        Text("MeetMe")
             .padding()
             .foregroundColor(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)))
             .background(Color(#colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)))
     }
 }
-
+// 09.01.2021
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
