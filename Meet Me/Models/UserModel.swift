2e204c8be6f7fb4ed6ee56ad2bfbd2765099e445
lukas Dech <lukasdech@gmx.de>
2021-01-27 20:36:42 +0100

    added Model User dateqwe

---
+++ Meet Me/Models/UserModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserModel.swift
+//  Meet Me
+//
+//  Created by Lukas Dech on 25.01.21.
+//
+
+import Foundation
+
+struct UserModel: Codable {
+    var id: String?
+    var userId: String = ""
+    var name: String = ""
+    var age: Int = 0
+    
+    
+}
