b049735732456a7d3410a3d8a3bbcf7d2f1ffdb1
lukas Dech <lukasdech@gmx.de>
2021-01-29 20:33:12 +0100

    UerModel

--- Meet Me/Models/UserModel.swift
+++ Meet Me/Models/UserModel.swift
@@ -9,10 +9,10 @@ import Foundation
 
 struct UserModel: Codable {
     var id: String?
-    var userId: String = ""
-    var name: String = ""
-    var birthdayDate: String = ""
-    var gender: String = ""
+    var userId: String
+    var name: String
+    var birthdayDate: String
+    var gender: String
     
 
 }
--- Meet Me/ViewModels/ProfileCreationModel.swift
+++ Meet Me/ViewModels/ProfileCreationModel.swift
@@ -7,4 +7,32 @@
 
 import Foundation
 import Firebase
-import FirebaseFirestoreSwift
+//import FirebaseFirestoreSwift
+
+class ProfileCreationModel {
+    
+    private var db: Firestore
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func save(userModel: UserModel, completion: @escaping (Result<UserModel?, Error>) -> Void) {
+        
+        do {
+            let ref = try db.collection("users").addDocument(from: userModel)
+            ref.getDocument { (snapshot, error) in
+                guard let snapshot = snapshot, error == nil else {
+                    completion(.failure(error!))
+                    return
+                }
+                
+                let store = try? snapshot.data(as: UserModel.self)
+                completion(.succes(stoe))
+            }
+        } catch let error {
+                completion(.failure(error))
+            }
+        }
+    }
+
