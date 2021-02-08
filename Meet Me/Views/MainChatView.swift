7b510bc4385949b03735daf0edb3f437dd49d6f4
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-02-08 22:25:27 +0100

    adding the main navigation

---
+++ Meet Me/Views/MainChatView.swift
@@ -0,0 +1,20 @@
+//
+//  MainChatView.swift
+//  Meet Me
+//
+//  Created by Philipp Hemkemeyer on 08.02.21.
+//
+
+import SwiftUI
+
+struct MainChatView: View {
+    var body: some View {
+        Text("Everything related to chats is here")
+    }
+}
+
+struct MainChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainChatView()
+    }
+}
---
+++ Meet Me/Views/MainExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  MainExploreView.swift
+//  Meet Me
+//
+//  Created by Philipp Hemkemeyer on 08.02.21.
+//
+
+import SwiftUI
+
+struct MainExploreView: View {
+    var body: some View {
+        Text("Everything to find meets is here - Meet Me Market")
+    }
+}
+
+struct MainExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainExploreView()
+    }
+}
