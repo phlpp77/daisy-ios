264cce5fdc4bb80cf511bfde4ad1322ddffcf6f0
lukas Dech <lukasdech@gmx.de>
2021-02-14 19:05:37 +0100

    /added  comment

--- Meet Me/ViewModels/MeProfileViewModel.swift
+++ Meet Me/ViewModels/MeProfileViewModel.swift
@@ -60,6 +60,7 @@ class MeProfileViewModel: ObservableObject {
         firestoreFotoManager.getAllPhotosFromUser(completionHandler: { success in
             if success {
                 // yeah picture
+                if
                 print(URL(string: self.firestoreFotoManager.photoModel[0].url)!)
                 let url = URL(string: self.firestoreFotoManager.photoModel[0].url)!
                 self.userPictureURL = url
