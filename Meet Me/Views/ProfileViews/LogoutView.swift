d3e4657c18ad83e1280c2c622f68191acabdb046
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-02-24 19:03:07 +0100

    logout preparing

---
+++ Meet Me/Views/ProfileViews/LogoutView.swift
@@ -0,0 +1,20 @@
+//
+//  LogoutView.swift
+//  Meet Me
+//
+//  Created by Philipp Hemkemeyer on 24.02.21.
+//
+
+import SwiftUI
+
+struct LogoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoutView()
+    }
+}
