022f2d4fcda779e2392612e1aa6375c83840ee79
lukas Dech <lukasdech@gmx.de>
2021-03-04 21:21:36 +0100

    changed stock chat

--- Meet Me/Extra/GlobalFunctions.swift
+++ Meet Me/Extra/GlobalFunctions.swift
@@ -57,5 +57,5 @@ let stockEvent2: EventModel = EventModel(eventId: "009", userId: "008", name: "N
 let stockEventObject: EventModelObject = EventModelObject(eventModel: stockEvent, position: .constant(.zero))
 
 // MARK: stock chat for chat area
-let stockChat: ChatModel = ChatModel(chatId: "egal", eventCreatorId: "creatoRR", matchedUserId: "receiveRR", eventId: "egal2", messages: [MessageModel(userId: "egal3", timeStamp: Timestamp(date: Date()), messageText: "Test msg1"), MessageModel(userId: "egal3", timeStamp: Timestamp(date: Date()) , messageText: "Test msg2")])
+let stockChat: ChatModel = ChatModel(chatId: "egal", eventCreatorId: "creatoRR", matchedUserId: "receiveRR", eventId: "egal2", messages: [MessageModel(userId: "egal3", timeStamp: Timestamp(date: Date()), messageText: "Hey you got a match, speek with your meeter about your event seetings")])
 
