4bf57eeed51bbafa05606f6cc9bad2f4c49e5ef3
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-03-05 23:00:21 +0100

    new default tab is the marketplace

--- Meet Me/Views/MainControllerViews/MainControllerView.swift
+++ Meet Me/Views/MainControllerViews/MainControllerView.swift
@@ -11,26 +11,30 @@ import FirebaseAuth
 struct MainControllerView: View {
     
     @State var startProcessDone: Bool = false
+    @State var startTab = 2
     
     var body: some View {
         
         // shows the main screen if the startProcess (user-creation) is done AND logged into firebase
         if startProcessDone || checkUserAccForAutoLogin() {
-            TabView {
+            TabView(selection: $startTab) {
                 MainSettingsView(startProcessDone: $startProcessDone).tabItem {
                     Image(systemName: "person.circle")
                     Text("Profile")
                 }
+                .tag(1)
                 
                 MainExploreView().tabItem {
                     Image(systemName: "circle.dashed")
                     Text("Marketplace")
                 }
+                .tag(2)
                 
                 MainChatView().tabItem {
                     Image(systemName: "paperplane.circle")
                     Text("Chats")
                 }
+                .tag(3)
             }
         } else {
             StartView(startProcessDone: $startProcessDone)
