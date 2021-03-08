c99649b726c051275a34528541bfc598d8a74f6e
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-03-08 23:30:38 +0100

    update MeEvent new UI

--- Meet Me/Views/MarketplaceViews/MeEventN.swift
+++ Meet Me/Views/MarketplaceViews/MeEventN.swift
@@ -13,7 +13,7 @@ struct MeEventN: View {
     @Binding var event: EventModel
     
     // represents the eventStatus of a MeEvent
-    @State var eventStatus: EventStatus = .matched
+    @State var eventStatus: EventStatus = .notLiked
     
     // to configure the date which is showing in the second line of the row
     var dateFormatter: DateFormatter = {
@@ -101,34 +101,36 @@ struct MeEventN: View {
             .offset(x: 10.0, y: 60)
             
             
-            // MARK: - EventStatus indicator
-            ZStack {
-                
-                // MARK: Background behind the Symbol
-                BlurView(style: .systemThinMaterial)
-                    .frame(width: 36, height: 36)
-                    .overlay(
-                        Circle()
-                            .stroke(
-                                LinearGradient(
-                                            gradient: Gradient(stops: [
-                                        .init(color: Color(#colorLiteral(red: 0.7791666388511658, green: 0.7791666388511658, blue: 0.7791666388511658, alpha: 0.949999988079071)), location: 0),
-                                        .init(color: Color(#colorLiteral(red: 0.7250000238418579, green: 0.7250000238418579, blue: 0.7250000238418579, alpha: 0)), location: 1)]),
-                                            startPoint: UnitPoint(x: 0.9016393067273221, y: 0.10416647788375455),
-                                            endPoint: UnitPoint(x: 0.035519096038869824, y: 0.85416653880629)),
-                                lineWidth: 0.5
-                            )
-                    )
-                    .clipShape(
-                        Circle()
-                    )
-                
-                // MARK: Symbol which shows the eventStatus
-                Image(systemName: eventStatus == .liked ? "rosette" : eventStatus == .matched ? "lock" : "")
-                    .font(.system(size: 24))
-                    .foregroundColor(.accentColor)
+            // MARK: - EventStatus indicator, only showing if event is liked or matched
+            if eventStatus != .notLiked {
+                ZStack {
+                    
+                    // MARK: Background behind the Symbol
+                    BlurView(style: .systemThinMaterial)
+                        .frame(width: 36, height: 36)
+                        .overlay(
+                            Circle()
+                                .stroke(
+                                    LinearGradient(
+                                                gradient: Gradient(stops: [
+                                            .init(color: Color(#colorLiteral(red: 0.7791666388511658, green: 0.7791666388511658, blue: 0.7791666388511658, alpha: 0.949999988079071)), location: 0),
+                                            .init(color: Color(#colorLiteral(red: 0.7250000238418579, green: 0.7250000238418579, blue: 0.7250000238418579, alpha: 0)), location: 1)]),
+                                                startPoint: UnitPoint(x: 0.9016393067273221, y: 0.10416647788375455),
+                                                endPoint: UnitPoint(x: 0.035519096038869824, y: 0.85416653880629)),
+                                    lineWidth: 0.5
+                                )
+                        )
+                        .clipShape(
+                            Circle()
+                        )
+                    
+                    // MARK: Symbol which shows the eventStatus
+                    Image(systemName: eventStatus == .liked ? "rosette" : eventStatus == .matched ? "lock" : "")
+                        .font(.system(size: 24))
+                        .foregroundColor(.accentColor)
+                }
+                .offset(x: 70, y: -70)
             }
-            .offset(x: 70, y: -70)
             
             
         }
