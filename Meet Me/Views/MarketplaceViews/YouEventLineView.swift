913f8e00758a3026cb2538bc519413bea3047c88
phlpp77 <PhilippHemkemeyer@iCloud.com>
2021-03-25 22:24:37 +0100

    updated refresh button

--- Meet Me/Views/MarketplaceViews/YouEventLineView.swift
+++ Meet Me/Views/MarketplaceViews/YouEventLineView.swift
@@ -23,6 +23,8 @@ struct YouEventLineView: View {
     
     // index which event needs to be removed from the array/scrollview
     @State var eventRemoveIndex: Int = -1
+    @GestureState var longPress: Bool = false
+    @State var pressDone: Bool = false
     
     var body: some View {
         ZStack {
@@ -59,13 +61,13 @@ struct YouEventLineView: View {
                                     )
                                     
                                     
-//                                    .onTapGesture {
-//                                        tappedYouEvent = eventArray[eventIndex]
-//                                        withAnimation(.easeIn(duration: 0.1)) {
-//                                            showYouProfileView = true
-//                                        }
-//                                        
-//                                    }
+                                    .onTapGesture {
+                                        tappedYouEvent = eventArray[eventIndex]
+                                        withAnimation(.easeIn(duration: 0.1)) {
+                                            showYouProfileView = true
+                                        }
+                                        
+                                    }
                             }
                             
                             .frame(width: 250, height: 250)
@@ -77,16 +79,7 @@ struct YouEventLineView: View {
                         .id(UUID())
                         
                         // button at the end to refresh events
-                        Button(action: {
-                            // TODO: @bundi refresh code here
-                            
-                        }, label: {
-                            Text("REfresh")
-                                .font(.largeTitle)
-                                .frame(width: 175, height: 175)
-                                .modifier(offWhiteShadow(cornerRadius: 14))
-                                .padding(30)
-                        })
+                        refreshButton
                         
                     }
                 }
@@ -99,6 +92,8 @@ struct YouEventLineView: View {
             
             // loading screen deactivated
             //            LoadingView(showLoadingScreen: $loading)
+            boomBar
+            
         }
         .frame(height: 380)
         .onAppear {
@@ -120,6 +115,60 @@ struct YouEventLineView: View {
         
     }
     
+    // MARK: Button to get new events
+    var refreshButton: some View {
+        
+        
+        VStack {
+            
+            
+            
+            Text("REFRESH")
+                    .gradientForeground(gradient: secondaryGradient)
+                    .font(.largeTitle)
+    //                .frame(width: longPress ? 200 : 175, height: longPress ? 200 : 175)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .modifier(offWhiteShadow(cornerRadius: 14))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 14, style: .continuous)
+                            .trim(from: longPress ? 0 : 1, to: 1)
+                            .stroke(Color.secondary, style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round, miterLimit: 0, dash: [longPress ? 800 : 0, 1000], dashPhase: 0))
+                            
+                    )
+                    .padding(30)
+                    .gesture(LongPressGesture(minimumDuration: 2)
+                                .updating($longPress) { currentState, gestureState, transaction in
+                                    transaction.animation = Animation.easeInOut(duration: 2.0)
+                                    
+                                    gestureState = currentState
+                                    
+                                    
+                                    
+                                }
+                                .onEnded { value in
+                                    print("ended")
+                                    pressDone = true
+                                })
+                .frame(width: 250, height: 250, alignment: .center)
+            
+            Spacer()
+                
+        }
+        
+        
+    }
+    
+    
+    // MARK: BoomBar
+    var boomBar: some View {
+        Color.gray
+            .frame(maxWidth: pressDone ? .infinity : 0, maxHeight: .infinity)
+            .animation(.spring(response: 1, dampingFraction: 0.3, blendDuration: 0))
+            .onChange(of: pressDone, perform: { value in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {pressDone = false})
+            })
+    }
+        
 }
 
 struct EventLineView_Previews: PreviewProvider {
